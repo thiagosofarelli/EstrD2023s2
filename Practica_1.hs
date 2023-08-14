1aff525e5836eb2253ea8cfc1602c8b0fee155ea
Sofarelli, Thiago <sofarelli.thiago10@gmail.com>
2023-08-14 18:47:24 -0300

    Ejercicio 2.2 realizado.

--- Practica_1.hs
+++ Practica_1.hs
@@ -1,6 +1,6 @@
 -- Práctica de ejercicios # 1 - Tipos algebraicos
 
--- Ejercicio 2
+-- Ejercicio 2.1
 
 -- a
 
@@ -23,3 +23,21 @@ maxDelPar :: (Int, Int) -> Int
 maxDelPar (n, m) = if (n > m)
                    then n
                    else m
+
+-- Ejercicio 2.2
+
+-- ejemplo 1
+
+-- sucesor (sumar 8 1)
+
+-- ejemplo 2
+
+-- maxDelPar (divisionYResto (sumar 10 90) (sucesor 9))
+
+-- ejemplo 3
+
+-- sucesor (maxDelPar (sumar 3 6, sumar 3 4))
+
+-- ejemplo 4
+
+-- sumar 12 (-2)
