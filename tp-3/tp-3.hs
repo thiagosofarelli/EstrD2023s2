ac6cce4b0e8ff0011336aaef1da5920ad5e0fc8a
Sofarelli, Thiago <sofarelli.thiago10@gmail.com>
2023-08-28 21:53:46 -0300

    Avance en "Camino hacia el tesoro".

--- tp-3/tp-3.hs
+++ tp-3/tp-3.hs
@@ -31,6 +31,10 @@ sacar c (Bolita col cel) = if sonElMismoColor c col
                            then cel
                            else (Bolita col (sacar c cel)) 
 
+ponerN :: Int -> Color -> Celda -> Celda
+ponerN 0 _ cel = cel 
+ponerN n col cel = (Bolita col (ponerN (n-1) col cel))
+
 celda0 = Bolita Rojo (Bolita Azul (Bolita Rojo (Bolita Azul CeldaVacia)))
 celda1 = Bolita Azul (Bolita Azul CeldaVacia)
 
@@ -42,3 +46,41 @@ sonElMismoColor _ _ = False
 unoSiOCeroSiNo :: Bool -> Int 
 unoSiOCeroSiNo True = 1 
 unoSiOCeroSiNo _ = 0
+
+data Objeto = Cacharro | Tesoro
+    deriving Show
+data Camino = Fin | Cofre [Objeto] Camino | Nada Camino
+    deriving Show
+
+-- Ejercicio 1.2
+
+hayTesoro :: Camino -> Bool
+-- Indica si hay un cofre en el camino.
+hayTesoro Fin = False
+hayTesoro (Nada cam) = hayTesoro cam
+hayTesoro (Cofre objetos cam) = hayTesoroEn' objetos || hayTesoro cam
+
+camino1 = Cofre [Cacharro] (Cofre [Cacharro] (Cofre [Cacharro, Tesoro] Fin))
+
+hayTesoroEn' :: [Objeto] -> Bool
+hayTesoroEn' [] = False
+hayTesoroEn' (o:os) = esTesoro o || hayTesoroEn' os
+
+esTesoro :: Objeto -> Bool
+esTesoro Tesoro = True
+esTesoro _ = False
+
+pasosHastaTesoro :: Camino -> Int
+--Indica la cantidad de pasos que hay que recorrer hasta llegar al primer cofre con un tesoro.
+--Si un cofre con un tesoro está al principio del camino, la cantidad de pasos a recorrer es 0.
+--Precondición: tiene que haber al menos un tesoro
+pasosHastaTesoro Fin = 0
+pasosHastaTesoro (Nada cam) = 1 + pasosHastaTesoro cam
+pasosHastaTesoro (Cofre objetos cam) = if hayTesoroEn' objetos
+                                       then 0 
+                                       else 1 + pasosHastaTesoro cam
+
+hayTesoroEn :: Int -> Camino -> Bool
+--Indica si hay un tesoro en una cierta cantidad exacta de pasos. Por ejemplo, si el número de
+--pasos es 5, indica si hay un tesoro en 5 pasos.
+hayTesoroEn n cam = pasosHastaTesoro cam == n       
