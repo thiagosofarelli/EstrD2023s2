d695927afe5de713a73532e2e90d858b9e95b559
Sofarelli, Thiago <sofarelli.thiago10@gmail.com>
2023-09-19 10:59:16 -0300

    QueueV3 finalizada.-

---
+++ tp-5/QueueV3.hs
@@ -0,0 +1,42 @@
+module QueueV3 (
+    Queue, emptyQ, isEmptyQ, enqueue, firstQ, dequeue
+) where
+
+data Queue a = Queue [a] [a]
+            -- Cola  Fs  Bs
+    deriving Show
+{--
+Inv. Rep.: Si Fs es vacía, Bs es vacía.
+--}
+
+emptyQ :: Queue a
+--Crea una cola vacía.
+emptyQ = Queue [] []
+
+isEmptyQ :: Queue a -> Bool
+--Dada una cola indica si la cola está vacía.
+isEmptyQ (Queue fs _) = null fs
+
+enqueue :: a -> Queue a -> Queue a
+--Dados un elemento y una cola, agrega ese elemento a la cola.
+enqueue x (Queue fs bs) = (Queue  fs (x:bs))
+
+firstQ :: Queue a -> a
+--Dada una cola devuelve el primer elemento de la cola
+--Es parcial, la cola debe tener al menos un elemento.
+firstQ (Queue fs _) = head fs
+
+dequeue :: Queue a -> Queue a
+--Dada una cola la devuelve sin su primer elemento.
+dequeue (Queue fs bs) = if null fs || null (tail fs)
+                      then (Queue (bs) [])
+                      else (Queue (tail fs) bs)
+
+queueEx = Queue [3] [5,7,89]
+
+reversa :: [a] -> [a]
+reversa []      = []
+reversa (x:xs)  = reversa xs ++ [x]
+
+
+
