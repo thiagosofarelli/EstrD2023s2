f6291d105c8b85d166bfa3e8b30646e3492003e4
Sofarelli, Thiago <sofarelli.thiago10@gmail.com>
2023-10-26 21:18:38 -0300

    Avance en p9.-

--- tp-9/ejercicio4.cpp
+++ tp-9/ejercicio4.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+#include <iomanip>
+#include <string>
 // Ejercicio 4
 
 // Dar dos implementaciones para las siguientes funciones, una iterativa y otra recursiva, y utilizando
@@ -30,6 +32,7 @@ void cuentaRegresiva(int n) {
     for (; n >= 0; n--) {
         cout << n << endl;   
     }
+    cout << endl;
 }
 
 void cuentaRegresivaR(int n){
@@ -37,30 +40,103 @@ void cuentaRegresivaR(int n){
         cout << n << endl;
         cuentaRegresivaR(n-1);
     }
+    cout << endl;
 }
 
-/*
 // 3. 
-void desdeCeroHastaN(int n)
+void desdeCeroHastaN(int n) {
 // Propósito: imprime los números de 0 hasta n, separados por saltos de línea.
+    for (int i = 0; n >= i; i++) {
+        cout << i << endl;   
+    }
+    cout << endl;
+}
+
+void desdeCeroHastaNR(int n){
+    int i = 0;
+    if (n>=0) {
+        cout << i << endl;
+        i++;
+        desdeCeroHastaNR(n);
+    }
+    cout << endl;
+}
+
 
 // 4. 
-int mult(int n, int m)
-// Propósito: realiza la multiplicación entre dos números (sin utilizar la operación * de C++).
+int mult(int n, int m) {
+    // Propósito: realiza la multiplicación entre dos números (sin utilizar la operación * de C++).
+    int resultado = 0; 
+    for (; m > 0; m--) {
+        resultado += n; 
+    }
+    return resultado; 
+}
+
+int multR(int n, int m) {
+    if (m == 0) {
+        return 0;
+    } else if (m == 1) {
+        return n;
+    } else {
+        return n + multR(n, m - 1);
+    }
+}
+
 
 // 5. 
-void primerosN(int n, string s)
+void primerosN(int n, string s) {
 // Propósito: imprime los primeros n char del string s, separados por un salto de línea.
 // Precondición: el string tiene al menos n char.
+for (int i = 0; n > i; i++) {
+        cout << s[i] << endl;   
+    }
+    cout << endl;
+}
+
+void primerosNR(int n, string s){
+    int i = 0;
+    while (i < n){
+        cout << s[i] << endl;
+        i++;
+    }
+    cout << endl;
+}
+
 
 // 6. 
-bool pertenece(char c, string s)
+bool pertenece(char c, string s) {
 // Propósito: indica si un char c aparece en el string s.
+    for (int i = 0; i < s.length(); i++) {
+        if (c == s[i]) {
+            return true;
+        }
+    } 
+            return false;      
+}
+
+bool perteneceRAux(char c, string s, int n) {
+    if (n == 0) {
+        return false;
+    }
+    if (c == s[n - 1]) {
+        return true;
+    }
+    return perteneceRAux(c, s, n - 1);
+}
+
+bool perteneceR(char c, string s) {
+    return perteneceRAux(c, s, s.length());
+}
+
 
 // 7. 
 int apariciones(char c, string s)
 // Propósito: devuelve la cantidad de apariciones de un char c en el string s.
-*/
+
+
+
+
 int main() {
     // Prueba de printN 
     printN(5, "Hola");
@@ -70,4 +146,33 @@ int main() {
     cuentaRegresiva(10);
     // Prueba de cuentaRegresivaR
     cuentaRegresivaR(5);
+    // Prueba de desdeCeroHastaN
+    desdeCeroHastaN(3);
+    // Prueba de desdeCeroHastaNR
+    desdeCeroHastaN(3);
+    // Prueba de mult
+    cout << mult(45, 108) << endl;
+    cout << endl;
+    // Prueba de multR
+    cout << multR(22, 10) << endl;
+    cout << endl;
+    // Prueba de primerosN
+    primerosN(3, "Thiago");
+    // Prueba de primerosNR
+    primerosNR(3, "Thiago");
+    // Prueba de pertenece
+    cout << std::boolalpha;
+    cout << pertenece('a', "Thiago") << endl;
+    // Prueba de perteneceR
+    cout << std::boolalpha;
+    cout << perteneceR('o', "Thiago") << endl;
+    cout << endl;
+    // OTRAS PRUEBAS
+    string nombre = "Thiago"; // Tiene 6 caracteres, pero van del 0 al 5.
+    cout << nombre.length() << endl; // Devuelve 6
+    char primerCaracter = nombre[0]; // Devuelve 'T'
+    char ultimoCaracter = nombre[5]; // Devuelve 'o'
+    cout << primerCaracter << endl;
+    cout << ultimoCaracter << endl;
+    cout << endl;
 }
